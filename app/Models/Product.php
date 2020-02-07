9b6b9e25fe3d8e547d92c115dcbc2ce703eb9d3e
wakibul <wakibulh@gmail.com>
2020-02-07 18:22:15 +0530

    status

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     public function productPackage(){
-    	return $this->hasMany('App\Models\ProductPackage','product_id','id');
+    	return $this->hasMany('App\Models\ProductPackage','product_id','id')->where('status',1);
     }
 }
--- app/Models/ProductPackage.php
+++ app/Models/ProductPackage.php
@@ -8,7 +8,7 @@ class ProductPackage extends Model
 {
     //
     public function packageMaster(){
-    	return $this->belongsTo('App\Models\PackageMaster','package_masters_id','id');
+    	return $this->belongsTo('App\Models\PackageMaster','package_masters_id','id')->where('status',1);
     }
 
 
