ece592a3174d6e0513ba9a5022a96404c30afdf8
wakibul <wakibulh@gmail.com>
2020-03-17 14:12:55 +0530

    price

--- app/Http/Controllers/Api/Customer/OrderController.php
+++ app/Http/Controllers/Api/Customer/OrderController.php
@@ -25,7 +25,7 @@ class OrderController extends Controller
     {
         //
         $user_id = auth('api')->user()->id;
-        $orders = Order::select('id','order_confirm_id','otp','total_price_with_tax','time_slot_id','created_at','status')->with('timeSlot:id,slot','orderTransaction:id,order_id,quantity,product_id,product_package_id','orderTransaction.product:id,name','orderTransaction.productPackage:id,product_id,package_masters_id,market_price,offer_price,offer_percentage,is_offer','orderTransaction.productPackage.packageMaster:id,name')->where('user_id',$user_id)->orderBy('id','desc')->withTrashed()->paginate(30);
+        $orders = Order::select('id','order_confirm_id','otp','total_price_with_tax','total_price','discount_amt','delivery_charge','time_slot_id','created_at','status')->with('timeSlot:id,slot','orderTransaction:id,order_id,quantity,product_id,product_package_id','orderTransaction.product:id,name','orderTransaction.productPackage:id,product_id,package_masters_id,market_price,offer_price,offer_percentage,is_offer','orderTransaction.productPackage.packageMaster:id,name')->where('user_id',$user_id)->orderBy('id','desc')->withTrashed()->paginate(30);
         if(!$orders->isEmpty())
             return response()->json(['success'=>true,'orders'=>$orders]);
         else
